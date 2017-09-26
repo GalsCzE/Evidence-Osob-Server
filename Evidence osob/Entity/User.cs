7e63fd9d941bc6e89496691425a531c82d0e7b4d
GalsCzE <sevca99@gmail.com>
2017-09-26 12:24:03 +0200

    26.9

---
+++ Evidence osob/Entity/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evidence_osob.Entity
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Birth_number { get; set; }
+        public int Date_number { get; set; }
+        public string Gender { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " " + Surname + " " + Birth_number + " " + Date_number + " " + Gender;
+        }
+    }
+}
