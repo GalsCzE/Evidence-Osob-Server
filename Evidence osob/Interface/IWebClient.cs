f6180c18e1827bf6caf224b7518ef581b8251330
GalsCzE <sevca99@gmail.com>
2017-09-26 12:41:10 +0200

    26.9

---
+++ Evidence osob/Interface/IWebClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Evidence_osob.Entity;
+
+namespace Evidence_osob.Interface
+{
+    interface IWebClient
+    {
+        Task<List<User>> GetUserListAsync();
+    }
+}
--- Evidence osob/WebClient/Task.cs
+++ Evidence osob/WebClient/Task.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Evidence_osob.Interface;
+using Evidence_osob.Entity;
 
 namespace Evidence_osob.WebClient
 {
-    class Task
+    class Task : IWebClient
     {
+        public async Task<List<User>> GetUserListAsync()
+        {
+        }
     }
 }
